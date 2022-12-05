3e5490f8a3787d3261fa048e2e4f089edc10dc98
Edoardo Fiocchi <edokyactus@gmail.com>
2022-12-05 17:28:51 +0100

    style: Added other checks in main

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,26 @@ mod day4;
 mod day5;
 
 fn main() {
-    let arg = std::env::args().nth(1);
-    match arg {
+    
+    let arg;
+    match std::env::args().nth(1) {
         None => {
             println!("Usage: aoc2022 <day> [ex]");
-            println!("If you are using cargo: cargo run <day> [ex]");
+            println!("With cargo: cargo run <day> [ex]");
+            std::process::exit(1);
+        },
+        Some(v) => arg = v
+    }
+
+    let day;
+    match arg.parse::<u32>() {
+        Ok(d) => day = d,
+        Err(_) => {
+            println!("Day must be an integer from 1 to 25.");
             std::process::exit(1);
         }
-        _ => ()
     }
-    let arg = arg.unwrap();
-    let day = arg.parse::<i32>().unwrap();
+
     let example = std::env::args().nth(2) == Some("ex".to_string());
     match day {
         1 => day1::day1_solution(example),
@@ -24,6 +33,9 @@ fn main() {
         3 => day3::day3_solution(example),
         4 => day4::day4_solution(example),
         5 => day5::day5_solution(example),
-        _ => println!("Day {} not implemented", day),
+        _ => {
+            println!("Day {} not implemented", day);
+            std::process::exit(1);
+        }
     }
 }
