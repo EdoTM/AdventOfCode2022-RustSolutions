741f14d2b04bdb249d0bbd0e4273dd70c9f79015
Edoardo Fiocchi <edokyactus@gmail.com>
2022-12-12 11:50:42 +0100

    feat: Added functions for calculating gcd and lcm

--- src/utils.rs
+++ src/utils.rs
@@ -17,3 +17,26 @@ pub fn print_part_solution<T: std::fmt::Display>(part: u8, solution: T) {
     println!("Part {} solution:", part);
     println!("{}", solution);
 }
+
+
+pub mod math {
+    pub fn gcd(a: u32, b: u32) -> u32 {
+        let (mut a, mut b) = (a, b);
+        while b != 0 {
+            (a, b) = (b, a % b);
+        }
+
+        a
+    }
+
+    pub fn lcm_multiple(values: &mut impl Iterator<Item = u32>) -> Option<u64> {
+        let mut _gcd = values.next()?;
+        let mut prod = _gcd as u64;
+        for value in values {
+            _gcd = gcd(_gcd, value);
+            prod *= value as u64;
+        }
+
+        Some(prod / _gcd as u64)
+    }
+}
